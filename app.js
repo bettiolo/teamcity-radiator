c60f1bb4d8411a9dbf2bcf604b321612a3779601
Marco Bettiolo <marco.bettiolo@7digital.com>
2014-05-20 14:36:05 +0100

    removed unneded complexity

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var debug = require('debug')('app');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+// var logger = require('morgan');
+// var cookieParser = require('cookie-parser');
+// var bodyParser = require('body-parser');
 
 var routes = require('./routes');
 
@@ -14,13 +14,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(require('node-compass')({mode: 'expanded'}));
+app.use(favicon(path.join(__dirname, 'public/favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded());
+// app.use(cookieParser());
+// app.use(require('node-compass')({mode: 'expanded'}));
+
 
 app.use(routes);
 

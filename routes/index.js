b6bf17839ca0f70d4f8a0bde1b0ae8debd91d2df
Marco Bettiolo <marco.bettiolo@7digital.com>
2014-05-20 11:20:47 +0100

    Added index page, removed forever for start, added dev script

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 var util = require('util');
+
 var setupBuildsRoutes = require('./builds');
 var setupFooRoutes = require('./foo');
-
 setupFooRoutes(router);
 setupBuildsRoutes(router);
+
+router.get('/', function(req, res) {
+  res.status(200).send('<h1>Hello World</h1>');
+});
 
 module.exports = router;

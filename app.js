545ab50da15901e80f971455d6e47391f7d2b75e
Marco Bettiolo <marco.bettiolo@7digital.com>
2014-05-20 12:42:00 +0100

    More error logging

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+// if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
@@ -43,20 +43,22 @@ if (app.get('env') === 'development') {
             error: err
         });
     });
-}
+// }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//         message: err.message,
+//         error: {}
+//     });
+// });
 
 process.on('uncaughtException', function(err) {
   debug('Caught exception: ' + err);
+  debug(err.stack);
+  process.exit(1);
 });
 
 module.exports = app;

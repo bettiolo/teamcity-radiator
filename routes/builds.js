6c0f3c0043ad32edcf48dc25465b2dee05b4e094
Marco Bettiolo <marco.bettiolo@7digital.com>
2014-05-20 18:17:25 +0100

    work in progress in separating logic added some css

--- routes/builds.js
+++ routes/builds.js
@@ -1,6 +1,7 @@
 var debug = require('debug')('/builds');
 var buildTypesClient = require('../lib/buildTypesClient');
 var buildsClient = require('../lib/buildsClient');
+var _ = require('lodash');
 
 module.exports = function setupRoute(router) {
     "use strict";
@@ -9,17 +10,36 @@ module.exports = function setupRoute(router) {
     router.get('/:server/:projectPrefix?', function(req, res) {
         var server = req.params.server;
         var projectPrefix = req.params.projectPrefix;
-        buildTypesClient.getAll(server, projectPrefix, function (allBuildTypes) {
-            buildsClient.getFailed(server, allBuildTypes, function (failedBuilds) {
-                buildTypesClient.sortBuildTypesByLatestBuild(failedBuilds, function(sortedFailedBuids) {
-                    res.render('builds', {
-                        title: 'Express',
-                        server: server,
-                        projectPrefix: projectPrefix,
-                        builds: sortedFailedBuids,
-                        updated: new Date().getTime()
+        buildTypesClient.getAll(server, projectPrefix, function (buildTypes) {
+            var buildTypeIds = _.map(buildTypes, function (buildType) { return buildType.id });
+            buildsClient.getFailed(server, buildTypeIds, function (failedBuilds) {
+
+                var failedBuildTypes = [];
+                var sortedFailedBuilds = _(failedBuilds)
+                    .sortBy(function (failedBuild) {
+                        return failedBuild.id;
+                    })
+                    .reverse()
+                    .value();
+
+                _.forEach(sortedFailedBuilds, function (failedBuild) {
+                    var failedBuildType = _.find(buildTypes, function (buildType) {
+                       return buildType.id == failedBuild.buildTypeId
                     });
+                    failedBuildType.latestBuild = failedBuild;
+                    failedBuildTypes.push(failedBuildType);
                 });
+
+                debug('Total build types found: ' + buildTypes.length);
+
+                res.render('builds', {
+                    title: 'Express',
+                    server: server,
+                    projectPrefix: projectPrefix,
+                    builds: failedBuildTypes,
+                    updated: new Date().getTime()
+                });
+
             })
         });
     });

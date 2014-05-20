2e2420eb3fdbb5953a4b2eb2d7ff0dfa763f767d
Marco Bettiolo <marco.bettiolo@7digital.com>
2014-05-20 17:08:23 +0100

    work in progress in separating logic

--- routes/builds.js
+++ routes/builds.js
@@ -1,71 +1,24 @@
 var debug = require('debug')('/builds');
-var request = require('request-json');
-var util = require('util');
-var _ = require('lodash');
 var buildTypes = require('../lib/buildTypes');
-
-function getBuild(server, id, callback) {
-    // ?locator=status:error,status:running
-    // ?locator=status:failure
-    // http://<server>/httpAuth/app/rest/buildQueue
-    // ?lookupLimit=1
-
-    // last build per client
-    // /guestAuth/app/rest/buildTypes/id:ApiTeam_ApiLiveEnvironmentTests_GZipEnabled/builds/?locator=lookupLimit:1
-    var serverUrl = 'http://' + server;
-    var path = '/guestAuth/app/rest/buildTypes/id:' + id + '/builds/?locator=lookupLimit:1';
-    debug('GET ' + serverUrl + path);
-    var client = request.newClient(serverUrl);
-    client.get(path,
-        function parseBuilds(error, response, body) {
-            if (error) {
-              debug('Error:');
-              debug(util.inspect(error));
-            }
-//            debug('Body:');
-//            debug(util.inspect(body));
-            if (!error && response.statusCode == 200) {
-                callback(body.build);
-            } else {
-                callback({});
-            }
-        }
-    )
-}
-
-
+var builds = require('../lib/builds');
 
 module.exports = function setupRoute(router) {
     "use strict";
     /* GET home page. */
-    router.get('/:server/:projectPrefix?', function(req, res) {
-        buildTypes.getAll(req.params.server, req.params.projectPrefix, function (builds) {
 
-            var failedBuilds = [];
-            var buildTypesCount = builds.length;
-            var finished = _.after(buildTypesCount, function () {
+    router.get('/:server/:projectPrefix?', function(req, res) {
+        var server = req.params.server;
+        var projectPrefix = req.params.projectPrefix;
+        buildTypes.getAll(server, projectPrefix, function (buildTypes) {
+            builds.getFailed(server, buildTypes, function (failedBuilds) {
                 res.render('builds', {
                     title: 'Express',
-                    server: req.params.server,
-                    projectPrefix: req.params.projectPrefix,
-                    builds: _(failedBuilds)
-                        .sortBy(function (failedBuild) {
-                            return failedBuild.latestBuild.id;
-                        })
-                        .reverse()
-                        .value()
+                    server: server,
+                    projectPrefix: projectPrefix,
+                    builds: failedBuilds,
+                    updated: new Date()
                 });
-            });
-            _.forEach(builds, function (buildType) {
-                getBuild(req.params.server, buildType.id, function(build) {
-                    if (build && build[0] && build[0].status != 'SUCCESS') {
-                        debug(util.inspect(build));
-                        buildType.latestBuild = build[0];
-                        failedBuilds.push(buildType);
-                    }
-                    finished();
-                });
-            });
+            })
         });
     });
 };

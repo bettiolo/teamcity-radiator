87a65e59c737365651ee21b47701a7c0247d7818
Marco Bettiolo <marco@bettiolo.it>
2015-01-26 20:56:42 +0000

    Added support for investigation

--- routes/builds.js
+++ routes/builds.js
@@ -2,6 +2,7 @@ var uuid = require('node-uuid');
 var debug = require('debug')('/builds');
 var buildTypesClient = require('../lib/buildTypesClient');
 var buildsClient = require('../lib/buildsClient');
+var investigations = require('../lib/investigations');
 var _ = require('lodash');
 
 module.exports = function setupRoute(router) {
@@ -11,44 +12,51 @@ module.exports = function setupRoute(router) {
   router.get('/:server/:projectPrefix?', function (req, res) {
     var server = req.params.server;
     var projectPrefix = req.params.projectPrefix;
-    buildTypesClient.getAll(server, projectPrefix, function (buildTypes) {
-      var buildTypeIds = _.map(buildTypes, function (buildType) {
-        return buildType.id
-      });
-      buildsClient.getFailed(server, buildTypeIds, function (failedBuilds) {
-
-        var failedBuildTypes = [];
-        var sortedFailedBuilds = _(failedBuilds)
-          .sortBy(function (failedBuild) {
-            return failedBuild.id;
-          })
-          .reverse()
-          .value();
-
-        _.forEach(sortedFailedBuilds, function (failedBuild) {
-          var failedBuildType = _.find(buildTypes, function (buildType) {
-            return buildType.id == failedBuild.buildTypeId
-          });
-          failedBuildType.latestBuild = failedBuild;
-          failedBuildTypes.push(failedBuildType);
+    investigations.get(server, projectPrefix, function(err, investigations) {
+      // if (err) ???
+      buildTypesClient.getAll(server, projectPrefix, function (buildTypes) {
+        var buildTypeIds = _.map(buildTypes, function (buildType) {
+          return buildType.id
         });
+        buildsClient.getFailed(server, buildTypeIds, function (failedBuilds) {
+          var failedBuildTypes = [];
+          var sortedFailedBuilds = _(failedBuilds)
+            .sortBy(function (failedBuild) {
+              return failedBuild.id;
+            })
+            .reverse()
+            .value();
 
-        debug('Total build types found: ' + buildTypes.length);
+          _.forEach(sortedFailedBuilds, function (failedBuild) {
+            var failedBuildType = _.find(buildTypes, function (buildType) {
+              return buildType.id === failedBuild.buildTypeId
+            });
+            failedBuildType.latestBuild = failedBuild;
+            if (investigations[failedBuildType.id]) {
+              failedBuildType.assignee = investigations[failedBuildType.id];
+            }
+            debug(failedBuildType.assignee);
+            failedBuildTypes.push(failedBuildType);
+          });
 
-        var buildStatus = {
-          title: 'TeamCity Radiator',
-          server: server,
-          projectPrefix: projectPrefix,
-          updated: new Date()
-        };
-        if (sortedFailedBuilds.length > 0) {
-          buildStatus.builds = failedBuildTypes;
-          res.render('builds', buildStatus);
-        } else {
-          buildStatus.random = uuid.v4();
-          res.render('cats', buildStatus);
-        }
-      })
+          debug('Failed builds: ' + buildTypes.length);
+          debug('Total investigations: ' + investigations.length);
+
+          var buildStatus = {
+            title: 'TeamCity Radiator',
+            server: server,
+            projectPrefix: projectPrefix,
+            updated: new Date()
+          };
+          if (sortedFailedBuilds.length > 0) {
+            buildStatus.builds = failedBuildTypes;
+            res.render('builds', buildStatus);
+          } else {
+            buildStatus.random = uuid.v4();
+            res.render('cats', buildStatus);
+          }
+        })
+      });
     });
   });
 };
